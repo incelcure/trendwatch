8591ea677c55e1e32f8ffa8ac4018261cddc4633
harpoon <sergeekoilia99@gmail.com>
2025-05-09 19:06:15 +0300

    fix: kafka ports and load job

--- jobs/load/load.py
+++ jobs/load/load.py
@@ -4,11 +4,14 @@ spark = SparkSession.builder \
     .appName("SendToKafka") \
     .getOrCreate()
 
-# Читаем все parquet-файлы рекурсивно
 df = spark.read.option("recursiveFileLookup", "true").parquet("s3a://trendwatch/processed_data/")
 
-# Преобразуем строки в JSON-объекты
 json_df = df.selectExpr("CAST(to_json(struct(*)) AS STRING) AS value")
+
+print("--------------------------DATA FRAME INFO--------------------------\n\n")
+print(df.count)
+print("\n\n--------------------------DATA FRAME INFO--------------------------")
+
 
 # Пишем в Kafka-топик
 json_df.write \
